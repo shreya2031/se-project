14f2d407576e9a51a47774fcc09d842265d0d0c3
Sahithi Venkatesan <sahi.48.3@gmail.com>
2021-04-19 19:11:19 +0530

    Added Navbar and added reactstrap package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
 
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" light expand="md">
+        <NavbarBrand href="/" style={{color: "white"}}>reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/" style={{color: "white"}}>Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/Sahithi-Venkatesan/se-project" style={{color: "white"}}>GitHub</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText style={{color: "white"}}>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Example />
     </div>
   );
 }
