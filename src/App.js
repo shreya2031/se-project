5660ca359d70cbb834667eb1db7e7645a53e3871
Sahithi Venkatesan <sahi.48.3@gmail.com>
2021-04-26 16:02:20 +0530

    React-player...seek and play aren't working properly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
+import { Component } from 'react'
+import ReactPlayer from 'react-player'
 import {
   Collapse,
   Navbar,
@@ -10,8 +12,10 @@ import {
   NavLink,
   NavbarText
 } from 'reactstrap';
+import { Toast, ToastBody } from 'reactstrap';
 
-const Example = (props) => {
+
+const NavComponent = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
@@ -33,15 +37,282 @@ const Example = (props) => {
           <NavbarText style={{color: "white"}}>Simple Text</NavbarText>
         </Collapse>
       </Navbar>
-    </div>
+    </div>  
   );
 }
+
+const ToastCard = (props) => {
+  return (
+    <div className="toast_body">
+        <Toast>
+          <ToastBody>
+            Components to be placed here!
+          </ToastBody>
+        </Toast>
+    </div>
+  );
+};
+
+
+class App extends Component {
+  state = {
+    url: ['https://soundcloud.com/kinabeats/kina-can-we-kiss-foreverfeat-adriana-proenza?in_system_playlist=charts-top%3Apop%3Ain','https://soundcloud.com/tycho/tycho-awake'] ,
+    pip: false,
+    playing: false,
+    controls: false,
+    light: false,
+    volume: 0.8,
+    muted: false,
+    played: 0,
+    loaded: 0,
+    duration: 0,
+    playbackRate: 1.0,
+    loop: false
+  }
+
+  load = url => {
+    this.setState({
+      url,
+      played: 0,
+      loaded: 0,
+      pip: false
+    })
+  }
+
+  handlePlayPause = () => {
+    this.setState({ playing: !this.state.playing })
+  }
+
+  handleStop = () => {
+    this.setState({ url: null, playing: false })
+  }
+
+  handleToggleControls = () => {
+    const url = this.state.url
+    this.setState({
+      controls: !this.state.controls,
+      url: null
+    }, () => this.load(url))
+  }
+
+  handleToggleLight = () => {
+    this.setState({ light: !this.state.light })
+  }
+
+  handleToggleLoop = () => {
+    this.setState({ loop: !this.state.loop })
+  }
+
+  handleVolumeChange = e => {
+    this.setState({ volume: parseFloat(e.target.value) })
+  }
+
+  handleToggleMuted = () => {
+    this.setState({ muted: !this.state.muted })
+  }
+
+  handleSetPlaybackRate = e => {
+    this.setState({ playbackRate: parseFloat(e.target.value) })
+  }
+
+  handleTogglePIP = () => {
+    this.setState({ pip: !this.state.pip })
+  }
+
+  handlePlay = () => {
+    console.log('onPlay')
+    this.setState({ playing: true })
+  }
+
+  handleEnablePIP = () => {
+    console.log('onEnablePIP')
+    this.setState({ pip: true })
+  }
+
+  handleDisablePIP = () => {
+    console.log('onDisablePIP')
+    this.setState({ pip: false })
+  }
+
+  handlePause = () => {
+    console.log('onPause')
+    this.setState({ playing: false })
+  }
+
+  handleSeekMouseDown = e => {
+    this.setState({ seeking: true })
+  }
+
+  handleSeekChange = e => {
+    this.setState({ played: parseFloat(e.target.value) })
+  }
+
+  handleSeekMouseUp = e => {
+    this.setState({ seeking: false })
+    this.player.seekTo(parseFloat(e.target.value))
+  }
+
+  handleProgress = state => {
+    console.log('onProgress', state)
+    // We only want to update time slider if we are not currently seeking
+    if (!this.state.seeking) {
+      this.setState(state)
+    }
+  }
+
+  handleEnded = () => {
+    console.log('onEnded')
+    this.setState({ playing: this.state.loop })
+  }
+
+  handleDuration = (duration) => {
+    console.log('onDuration', duration)
+    this.setState({ duration })
+  }
+  renderLoadButton = (url) => {
+    return (
+      <button onClick={() => this.load(url)}>
+      </button>
+    )
+  }
+
+  ref = player => {
+    this.player = player
+  }
+
+  render () {
+    const { url, playing, controls, light, volume, muted, loop, played, loaded, playbackRate, pip } = this.state
+
+    return (
+      <div className='app'>
+         <NavComponent />
+        <section className='section'>
+          <div className='player-wrapper'>
+          <ReactPlayer
+              ref={this.ref}
+              className='react-player'
+              width='100%'
+              height='100%'
+              url={url[0]}
+              pip={pip}
+              playing={playing}
+              controls={controls}
+              light={light}
+              loop={loop}
+              playbackRate={playbackRate}
+              volume={volume}
+              muted={muted}
+              onReady={() => console.log('onReady')}
+              onStart={() => console.log('onStart')}
+              /*onPlay={this.handlePlay}*/
+              onEnablePIP={this.handleEnablePIP}
+              onDisablePIP={this.handleDisablePIP}
+              onPause={this.handlePause}
+              onBuffer={() => console.log('onBuffer')}
+              onSeek={e => console.log('onSeek', e)}
+              onEnded={this.handleEnded}
+              onError={e => console.log('onError', e)}
+              onProgress={this.handleProgress}
+              onDuration={this.handleDuration}
+            />       
+                 
+            <ReactPlayer
+              ref={this.ref}
+              className='react-player'
+              width='100%'
+              height='100%'
+              url={url[1]}
+              pip={pip}
+              playing={playing}
+              controls={controls}
+              light={light}
+              loop={loop}
+              playbackRate={playbackRate}
+              volume={volume}
+              muted={muted}
+              onReady={() => console.log('onReady')}
+              onStart={() => console.log('onStart')}
+              onPlay={this.handlePlay}
+              onEnablePIP={this.handleEnablePIP}
+              onDisablePIP={this.handleDisablePIP}
+              onPause={this.handlePause}
+              onBuffer={() => console.log('onBuffer')}
+              /*onSeek={e => console.log('onSeek', e)}*/z
+              onEnded={this.handleEnded}
+              onError={e => console.log('onError', e)}
+              onProgress={this.handleProgress}
+              onDuration={this.handleDuration}
+            />
+          </div>
+
+          <table>
+            <tbody>
+              <tr>
+                <th>Controls</th>
+                <td>
+                  <button onClick={this.handleStop}>Stop</button>
+                  <button onClick={this.handlePlayPause}>{playing ? 'pause' : 'play'}&#9654;&#65039;</button>
+                  <button onClick={this.handleClickFullscreen}>Fullscreen</button>
+                  {light &&
+                    <button onClick={() => this.player.showPreview()}>Show preview</button>}
+                  {ReactPlayer.canEnablePIP(url) &&
+                    <button onClick={this.handleTogglePIP}>{pip ? 'Disable PiP' : 'Enable PiP'}</button>}
+                </td>
+              </tr>
+              <tr>
+                <th>Speed</th>
+                <td>
+                  <button onClick={this.handleSetPlaybackRate} value={1}>1x</button>
+                  <button onClick={this.handleSetPlaybackRate} value={1.5}>1.5x</button>
+                  <button onClick={this.handleSetPlaybackRate} value={2}>2x</button>
+                </td>
+              </tr>
+              <tr>
+                <th>Seek</th>
+                <td>
+                  <input
+                    type='range' min={0} max={0.999999} step='any'
+                    value={played}
+                    onMouseDown={this.handleSeekMouseDown}
+                    onChange={this.handleSeekChange}
+                    onMouseUp={this.handleSeekMouseUp}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th>Volume</th>
+                <td>
+                  <input type='range' min={0} max={1} step='any' value={volume} onChange={this.handleVolumeChange} />
+                </td>
+              </tr>
+              <tr>
+              </tr>
+              <tr>
+                <th>Played</th>
+                <td><progress max={1} value={played} /></td>
+              </tr>
+              <tr>
+                <th>Loaded</th>
+                <td><progress max={1} value={loaded} /></td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <ToastCard />
+      </div>
+    )
+  }
+}
+
+/*
 function App() {
   return (
-    <div className="App">
-      <Example />
+    <div className="Nav">
+      <NavComponent />
+       <ToastCard />
     </div>
   );
 }
+*/
 
 export default App;
