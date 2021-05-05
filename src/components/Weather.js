a8233e5cbfa34947546c5f8b73916d6340f5a502
Sahithi Venkatesan <sahi.48.3@gmail.com>
2021-05-05 10:11:01 +0530

    Fixed navbar

---
+++ src/components/Weather.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Weather extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Weather-Based songs!</h2>
+        </div>
+    );
+  }
+}
+
+export default Weather;
